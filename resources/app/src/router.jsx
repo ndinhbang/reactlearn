6f8a26a9b51f9a29af324bfebb89a79b12c67a63
Nguyen Dinh Bang <bangnd@abilive.vn>
2023-09-07 17:15:53 +0700

    wip

---
+++ resources/app/src/router.jsx
@@ -0,0 +1,6 @@
+import { createHashRouter } from "react-router-dom";
+import routes from "@/routes/web"
+
+const router = createHashRouter(routes);
+
+export default router;
--- resources/app/vite.config.js
+++ resources/app/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath, URL } from 'url'
+import dns from 'dns'
+
+// @see: https://vitejs.dev/config/server-options.html#server-host
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command, mode, ssrBuild }) => {
+    // load .env
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        plugins: [react()],
+        resolve: {
+            alias: [
+                { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+            ],
+        },
+    }
 })
